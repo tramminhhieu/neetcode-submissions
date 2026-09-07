ef0282cc48a389c42bfcb961386282fdbc3d3d1b
tramminhhieu <tramminhhieu09112005@gmail.com>
2026-09-07 19:46:31 +0700

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for(String s : strs){
+            int[] counts = new int[26];
+            for(char c : s.toCharArray()){
+                counts[c - 'a']++;
+            }
+            StringBuilder keyBuilder = new StringBuilder();
+            for(int num : counts){
+                keyBuilder.append(num).append('#');
+            }
+            String key = keyBuilder.toString();
+            List<String> group = map.getOrDefault(key, new ArrayList<>());
+            group.add(s);
+            map.put(key, group);  
+        }
+        return new ArrayList(map.values());
+    }
+}
