19594d8df545b3e9ac322ca36e02c18fbe34772d
tramminhhieu <tramminhhieu09112005@gmail.com>
2026-09-09 16:39:22 +0700

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE, profit = 0;
+        for(int i = 0; i < prices.length; i++){
+            min = Math.min(min, prices[i]);
+            if((prices[i] - min) > profit){
+                profit = prices[i] - min;
+            }
+        }
+        return profit;   
+    }
+}
